84686edb23f737bbe5b954366b08935e8166726c
Afnan Al-Sahli <141231550+devAfnan@users.noreply.github.com>
2024-02-04 23:05:30 +0300

    DBMS_Lab1.sql

--- DBMS_Lab1.sql
+++ DBMS_Lab1.sql
@@ -1,3 +1,9 @@
+/* 
+Create "Course" database.
+Create "courses" Table with the following attributes:
+• Course_NO 
+• Course_Name
+*/
 CREATE DATABASE Course;
 CREATE TABLE courses(
 Course_NO int,
